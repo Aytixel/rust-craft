14366fd41226fca9cd1ef3afcb8b3176f59fa487
Aytixel <aytixel@gmail.com>
2023-03-18 01:21:48 +0100

    Add function to parse byte array from packet

--- src/data_type/byte.rs
+++ src/data_type/byte.rs
@@ -1,5 +1,7 @@
 pub trait FromByte {
     fn from_byte(&mut self) -> Result<i8, &'static str>;
+
+    fn from_byte_array(&mut self, length: usize) -> Result<Vec<u8>, &'static str>;
 }
 
 impl FromByte for Vec<u8> {
@@ -9,6 +11,14 @@ impl FromByte for Vec<u8> {
         }
 
         Ok(self.remove(0) as i8)
+    }
+
+    fn from_byte_array(&mut self, length: usize) -> Result<Vec<u8>, &'static str> {
+        if self.len() < length {
+            return Err("Not enough data to parse Byte Array");
+        }
+
+        Ok(self.drain(..length).collect())
     }
 }
 
@@ -31,6 +41,21 @@ mod tests {
         assert_eq!(vec![].from_byte(), Err("Not enough data to parse Byte"));
         assert_eq!(vec![1].from_byte().unwrap(), 1);
         assert_eq!(vec![89].from_byte().unwrap(), 89);
+    }
+
+    #[test]
+    fn from_byte_array() {
+        assert_eq!(
+            vec![].from_byte_array(1),
+            Err("Not enough data to parse Byte Array")
+        );
+        assert_eq!(vec![1, 3, 80].from_byte_array(3).unwrap(), vec![1, 3, 80]);
+        assert_eq!(
+            vec![89, 8, 80, 23, 234, 235, 9, 7, 23, 54]
+                .from_byte_array(10)
+                .unwrap(),
+            vec![89, 8, 80, 23, 234, 235, 9, 7, 23, 54]
+        );
     }
 
     #[test]
