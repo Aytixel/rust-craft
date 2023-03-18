f7818c12e39bfbbba9aa9a20f7a86fded5939a77
Aytixel <aytixel@gmail.com>
2023-03-18 01:08:01 +0100

    Start login with encryption

--- src/packet/login/encryption.rs
+++ src/packet/login/encryption.rs
@@ -15,15 +15,8 @@ impl EncryptionPacket {
     pub fn new(encryption_data: Rc<EncryptionData>) -> Packet {
         let mut data = "".to_packet_string();
 
-        //data.append(&mut (encryption_data.der_public_key.as_bytes().len() as i32).to_varint());
-        //data.append(&mut encryption_data.der_public_key.as_bytes().to_vec());
-
-        let rsa = Rsa::generate(1024).unwrap();
-        let mut der = rsa.public_key_to_der().unwrap();
-
-        data.append(&mut (der.len() as i32).to_varint());
-        data.append(&mut der);
-
+        data.append(&mut (encryption_data.der_public_key.len() as i32).to_varint());
+        data.append(&mut encryption_data.der_public_key.clone());
         data.append(&mut (encryption_data.token.len() as i32).to_varint());
         data.append(&mut encryption_data.token.clone());
 
