f05476e27048517043a2a8194f7f9b6dac77b1fd
Aytixel <aytixel@gmail.com>
2023-03-24 23:27:19 +0100

    Deserialize damage_type

--- src/datapack.rs
+++ src/datapack.rs
@@ -1,3 +1,4 @@
+mod damage_type;
 mod dimension_type;
 mod recipes;
 
@@ -5,6 +6,7 @@ use std::collections::HashMap;
 
 #[derive(Debug)]
 pub struct Datapack {
+    damage_type: HashMap<String, damage_type::DamageType>,
     dimension_type: HashMap<String, dimension_type::DimensionType>,
     recipes: HashMap<String, recipes::Recipes>,
 }
@@ -12,6 +14,9 @@ pub struct Datapack {
 impl Datapack {
     pub fn new() -> Result<Self, String> {
         Ok(Self {
+            damage_type: damage_type::DamageType::deserialize_folder(
+                "./data/minecraft/damage_type/",
+            )?,
             dimension_type: dimension_type::DimensionType::deserialize_folder(
                 "./data/minecraft/dimension_type/",
             )?,
