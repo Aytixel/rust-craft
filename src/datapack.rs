0bb7bd100d900a2530bd0a16163484f20816d9ae
Aytixel <aytixel@gmail.com>
2023-03-26 21:46:01 +0200

    Deserialize datapack
    For now,  we only deserialize in a very basic and inefficient way, it's temporary. Later we'll load only what we need, when we need it.

--- src/datapack.rs
+++ src/datapack.rs
@@ -1,53 +1,88 @@
-mod advancements;
-mod chat_type;
-mod damage_type;
-mod dimension_type;
-mod items;
-mod loot_tables;
-mod recipes;
-mod structures;
-mod tags;
-mod translate;
+use std::fs::{self, read_dir};
+use std::io::BufReader;
 
 use hashbrown::hash_map::HashMap;
+use quartz_nbt::io::Flavor;
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+#[serde(untagged)]
+pub enum File {
+    Nbt(quartz_nbt::NbtCompound),
+    Json(serde_json::Value),
+}
+
+#[derive(Debug)]
+pub struct Folder {
+    folders: HashMap<String, Folder>,
+    files: HashMap<String, File>,
+}
+
+impl Folder {
+    pub fn deserialize_folder(path: &str) -> Result<Folder, String> {
+        Folder::deserialize_folder_(path)
+    }
+
+    fn deserialize_folder_(path: &str) -> Result<Folder, String> {
+        let mut folder = Folder {
+            folders: HashMap::new(),
+            files: HashMap::new(),
+        };
+
+        for file in read_dir(path).unwrap() {
+            if let Ok(file) = file {
+                if file.file_type().unwrap().is_dir() {
+                    folder.folders.insert(
+                        file.path()
+                            .file_name()
+                            .unwrap()
+                            .to_str()
+                            .unwrap()
+                            .to_string(),
+                        Folder::deserialize_folder_(
+                            file.path().into_os_string().as_os_str().to_str().unwrap(),
+                        )?,
+                    );
+                } else {
+                    let file_name = file.file_name();
+                    let file_name = file_name.to_str().unwrap();
+                    let file = fs::File::open(file.path()).map_err(|e| e.to_string())?;
+                    let mut reader = BufReader::new(file);
+
+                    if file_name.ends_with(".json") {
+                        folder.files.insert(
+                            file_name[..file_name.len() - 5].to_string(),
+                            File::Json(serde_json::from_reader(reader).map_err(|e| e.to_string())?),
+                        );
+                    } else if file_name.ends_with(".nbt") {
+                        folder.files.insert(
+                            file_name[..file_name.len() - 4].to_string(),
+                            File::Nbt(
+                                quartz_nbt::io::read_nbt(&mut reader, Flavor::GzCompressed)
+                                    .map_err(|e| e.to_string())?
+                                    .0,
+                            ),
+                        );
+                    }
+                }
+            }
+        }
+
+        Ok(folder)
+    }
+}
 
 #[derive(Debug)]
 pub struct Datapack {
-    advancements: HashMap<String, advancements::Advancement>,
-    chat_type: HashMap<String, chat_type::ChatType>,
-    damage_type: HashMap<String, damage_type::DamageType>,
-    dimension_type: HashMap<String, dimension_type::DimensionType>,
-    loot_tables: HashMap<String, loot_tables::LootTable>,
-    recipes: HashMap<String, recipes::Recipes>,
-    structures: HashMap<String, structures::Structure>,
-    tags: HashMap<String, tags::Tag>,
+    data: Folder,
+    assets: Folder,
 }
 
 impl Datapack {
     pub fn new(path: &str) -> Result<Self, String> {
         Ok(Self {
-            advancements: advancements::Advancement::deserialize_json_folder(
-                (path.to_string() + "/advancements/").as_str(),
-            )?,
-            chat_type: chat_type::ChatType::deserialize_json_folder(
-                (path.to_string() + "/chat_type/").as_str(),
-            )?,
-            damage_type: damage_type::DamageType::deserialize_json_folder(
-                (path.to_string() + "/damage_type/").as_str(),
-            )?,
-            dimension_type: dimension_type::DimensionType::deserialize_json_folder(
-                (path.to_string() + "/dimension_type/").as_str(),
-            )?,
-            loot_tables: loot_tables::LootTable::deserialize_json_folder(
-                (path.to_string() + "/loot_tables/").as_str(),
-            )?,
-            recipes: recipes::Recipes::deserialize_json_folder(
-                (path.to_string() + "/recipes/").as_str(),
-            )?,
-            structures: structures::Structure::deserialize_nbt_folder(
-                (path.to_string() + "/structures/").as_str(),
-            )?,
-            tags: tags::Tag::deserialize_json_folder((path.to_string() + "/tags/").as_str())?,
+            data: Folder::deserialize_folder((path.to_string() + "data/").as_str())?,
+            assets: Folder::deserialize_folder((path.to_string() + "assets/").as_str())?,
         })
     }
 }
@@ -58,6 +93,6 @@ mod tests {
 
     #[test]
     fn new() {
-        println!("{:#?}", Datapack::new("./data/minecraft/").unwrap());
+        println!("{:#?}", Datapack::new("./").unwrap());
     }
 }
