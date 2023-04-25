1475cfd9c5f5a74e78b47572bb4077d9953f71c3
Aytixel <aytixel@gmail.com>
2023-04-25 22:05:50 +0200

    Comment out datapack loading code line

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::{rc::Rc, thread::sleep, time::Duration};
 use log::info;
 use server::Server;
 
-use crate::{datapack::Datapack, version_info::VersionInfo};
+use crate::version_info::VersionInfo;
 
 mod client;
 mod data_type;
@@ -16,7 +16,7 @@ fn main() -> Result<(), String> {
     env_logger::init();
 
     let version_info = Rc::new(VersionInfo::new()?);
-    let _datapack = Rc::new(Datapack::new("./data/minecraft/")?);
+    //let _datapack = Rc::new(Datapack::new("./")?);
     let mut server = Server::new("0.0.0.0:25565", version_info)?;
 
     info!("Server listening on: 0.0.0.0:25565");
